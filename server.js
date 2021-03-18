346b0857736272be423f9d6ea9d35d331da1c542
alyciagoode20 <alyciadgoode@gmail.com>
2021-03-18 00:27:03 -0500

    server stuff

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ if (cluster.isMaster) {
   });
 
 } else {
+  var supervisor = require('supervisor');
   const app = express();
   const port = process.env.PORT || 8000;
 
@@ -30,20 +31,12 @@ if (cluster.isMaster) {
   app.get('/', function(req, res) {
     process.on('uncaughtException', function(err) {
       console.error(err);
-      try {
-        var timer = setTimeout(function() {
-          process.exit(1);
-        }, 30000);
-        timer.unref();  // get rid of timer.
-        server.close();  // stop taking requests.
-        cluster.worker.disconnect();  // Tell master worker is dead and needs replacing.
-        res.statusCode = 500;
-        res.setHeader('content-type', 'text-plain');
-        res.end("Sorry, there was a problem");
-      }
-      catch(err2) {
-        console.err("Error sending 500!", er2.stack);
-      }
+      process.exit(1);
+      server.close();  // stop taking requests.
+      cluster.worker.disconnect();  // Tell master worker is dead and needs replacing.
+      res.statusCode = 500;
+      res.setHeader('content-type', 'text-plain');
+      res.end("Sorry, there was a problem");
     });
     res.sendFile(path.join(__dirname + '/public/index.html'));  // send index.html
   });
@@ -51,20 +44,12 @@ if (cluster.isMaster) {
   app.post('/get_path', function(req, res) {
     process.on('uncaughtException', function(err) {
       console.error(err);
-      try {
-        var timer = setTimeout(function() {
-          process.exit(1);
-        }, 30000);
-        timer.unref();  // get rid of timer.
-        server.close();  // stop taking requests.
-        cluster.worker.disconnect();  // Tell master worker is dead and needs replacing.
-        res.statusCode = 500;
-        res.setHeader('content-type', 'text-plain');
-        res.end("Sorry, there was a problem");
-      }
-      catch(er2) {
-        console.error("Error sending 500!", er2.stack);
-      }
+      process.exit(1);
+      server.close();  // stop taking requests.
+      cluster.worker.disconnect();  // Tell master worker is dead and needs replacing.
+      res.statusCode = 500;
+      res.setHeader('content-type', 'text-plain');
+      res.end("Sorry, there was a problem");
     });
     // console.log("REQUEST: " + req.body.name);
     const script = exec('./a.out');  // run a.out executable.
